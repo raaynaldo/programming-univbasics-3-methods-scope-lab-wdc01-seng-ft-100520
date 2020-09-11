57bbd8f6f1144648e09f4f69a80ad2f2352bad1f
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-11 23:47:54 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,5 +13,5 @@ def link
 end
 
 def all_phrases
-  puts "Thank You Mario! But Our Princess Is In Another Castle!\n/It's-a me, Mario!\n/"
+  puts "TIt's Dangerous To Go Alone! Take This.\nhank You Mario! But Our Princess Is In Another Castle!\n/It's-a me, Mario!\n/"
 end
